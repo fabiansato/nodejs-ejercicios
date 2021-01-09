3637747277507f42db233abf705e1bc62d86fcf6
fabiansato <40687901+fabiansato@users.noreply.github.com>
2021-01-09 12:39:54 -0300

    Update products.js

--- ejercicio-alta-nodejs-base/routes/products.js
+++ ejercicio-alta-nodejs-base/routes/products.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. */ 
+/*req es toda la información que nos llega
+el res es lo que yo devuelvo
+*/
 router.get('/', function (req, res, next) { /* si por el metodo get mandamos una barra responde con el metodo*/
-    res.send('respond with a resource');
+    const productos =[ //creo una constante con un json interno dentro del array
+        { 
+            id:1,
+            name: "Moto g"
+
+        }
+        {
+            id: 2,
+            name: "Moto X"
+
+        }
+    ]
+    res.json(productos); // le mandamos este array
 }); /* pero si mandamos otra cosa siempre mostrara el notfound ejemplo pagina/users/sarasa */
 
 module.exports = router;
